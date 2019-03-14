272ee1abc4918c1be0eccf0dca664c8b734b6af0
Mahfuzur Rahman <thehry01@gmail.com>
2019-03-14 17:41:59 +0600

    Party Access Modified

--- MHGoldenJuteApp/MHGoldenJuteApp/DAL/PartyAccess.cs
+++ MHGoldenJuteApp/MHGoldenJuteApp/DAL/PartyAccess.cs
@@ -36,6 +36,7 @@ namespace MHGoldenJuteApp.DAL
 
         public Party GetAPartyById(int partyId)
         {
+            Connection.Open();
             Party aParty=new Party();
             Query="Select * from Party where PartyId=@partyId";
             Command = new SqlCommand(Query, Connection);
@@ -61,6 +62,48 @@ namespace MHGoldenJuteApp.DAL
             Connection.Close();
 
             return aParty;
+        }
+
+        public List<Party> GetAllParty()
+        {
+            List<Party> listOfParty = new List<Party>();
+            Connection.Open();
+            Query = "Select * from Party order by PartyId";
+            Command = new SqlCommand(Query, Connection);
+                        Reader = Command.ExecuteReader();
+            while (Reader.Read())
+            {
+                Party aParty = new Party();
+                aParty.PartyId = (int)Reader["PartyId"];
+                aParty.PartyName = Reader["PartyName"].ToString();
+                aParty.PartyType = Reader["PartyType"].ToString();
+                aParty.ContactPerson = Reader["ContactPerson"].ToString();
+                aParty.Address = Reader["Address"].ToString();
+                aParty.PhoneNo = Reader["PhoneNo"].ToString();
+                aParty.MobileNo = Reader["MobileNo"].ToString();
+                aParty.OpeningBalance = (double)Reader["OpeningBalance"];
+                aParty.ImageUrl = Reader["ImageUrl"].ToString();
+                aParty.Email = Reader["Email"].ToString();
+                listOfParty.Add(aParty);
+
+            }
+            Reader.Close();
+            Connection.Close();
+            return listOfParty;
+        }
+
+        public bool IsPartyNameExist(string partyName,string partyType)
+        {
+            Connection.Open();
+            Query = "select count(PartyName) from tblUser where PartyName='@PartyName' and PartyType='@PartyType'";
+            Command = new SqlCommand(Query, Connection);
+            Command.Parameters.Clear();
+            Command.Parameters.AddWithValue("PartyName",partyName);
+            Command.Parameters.AddWithValue("PartyType", partyType);
+            int result = (int)Command.ExecuteScalar();
+            Connection.Close();
+            if (result > 0) return true;
+            else return false;
         }
     }
 }
